e61e0e02f23ddd15b23939f56c6f0d7a7b01e9ca
Karl <zerratar@gmail.com>
2021-06-20 17:02:55 +0200

    Allow using !where <playername> on the centralized bot

--- src/ROBot.Core/Twitch/Commands/Island.cs
+++ src/ROBot.Core/Twitch/Commands/Island.cs
@@ -1,4 +1,5 @@
 ﻿using RavenBot.Core.Handlers;
+using RavenBot.Core.Ravenfall.Models;
 using ROBot.Core.GameServer;
 using System.Threading.Tasks;
 
@@ -15,9 +16,26 @@ namespace ROBot.Core.Twitch.Commands
                 var connection = game.GetConnection(session);
                 if (connection != null)
                 {
-                    var player = session.Get(cmd.Sender);
+                    Player player = null;
+                    if (!string.IsNullOrEmpty(cmd.Arguments))
+                    {
+                        var username = cmd.Arguments;
+                        if (cmd.Arguments.StartsWith("@"))
+                        {
+                            username = username.Substring(1);
+                        }
+                        player = session.GetUserByName(username);
+                    }
+
+                    if (player == null)
+                    {
+                        player = session.Get(cmd.Sender);
+                    }
+
                     if (player != null)
+                    {
                         await connection.RequestIslandInfoAsync(player);
+                    }
                 }
             }
         }
