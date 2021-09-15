12098a48380fb608c0468c0595ed88fd28631c9b
Karl <zerratar@gmail.com>
2021-09-15 19:10:50 +0200

    Allow game admins or me Kappa; To force add players. Allowing admins to Kick a player, do adjustments and then add them back to the game.

---
+++ src/ROBot.Core/Twitch/Commands/Add.cs
@@ -0,0 +1,44 @@
+﻿using RavenBot.Core.Handlers;
+using RavenBot.Core.Ravenfall.Models;
+using ROBot.Core.GameServer;
+using System.Threading.Tasks;
+
+namespace ROBot.Core.Twitch.Commands
+{
+    public class Add : TwitchCommandHandler
+    {
+        public override async Task HandleAsync(IBotServer game, ITwitchCommandClient twitch, ICommand cmd)
+        {
+            if (!cmd.Sender.DisplayName.ToLower().Equals("zerratar") && !cmd.Sender.IsGameAdmin)
+            {
+                return;
+            }
+
+            var channel = cmd.Channel;
+            var session = game.GetSession(channel);
+            if (session != null)
+            {
+                var connection = game.GetConnection(session);
+                if (connection != null)
+                {
+
+                    if (string.IsNullOrEmpty(cmd.Arguments))
+                        return;
+
+                    var values = cmd.Arguments.Split(' ');
+                    if (values.Length <= 0)
+                        return;
+
+                    var player = session.GetUserByName(values[0]);
+                    if (player == null)
+                        return;
+
+                    if (values.Length > 1)
+                        player.Identifier = values[1];
+
+                    await connection.JoinAsync(player);
+                }
+            }
+        }
+    }
+}
