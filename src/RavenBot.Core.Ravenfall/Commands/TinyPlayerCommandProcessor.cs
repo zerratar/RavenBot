771a64c103b165dd1658313665b0b940d83621ef
Karl <zerratar@gmail.com>
2021-09-15 16:36:26 +0200

    Fix an issue with local bot not accepting certain channel point rewards chat commands

--- src/RavenBot.Core.Ravenfall/Commands/TinyPlayerCommandProcessor.cs
+++ src/RavenBot.Core.Ravenfall/Commands/TinyPlayerCommandProcessor.cs
@@ -10,7 +10,7 @@ namespace RavenBot.Core.Ravenfall.Commands
 
         public TinyPlayerCommandProcessor(IRavenfallClient game, IPlayerProvider playerProvider)
         {
-            //this.RequiresBroadcaster = true;
+            this.RequiresBroadcaster = true;
             this.game = game;
             this.playerProvider = playerProvider;
         }
