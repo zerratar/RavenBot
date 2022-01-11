2b45ead0bc83ab7b2c6d64cda3ed5d232109acf5
Karl <zerratar@gmail.com>
2022-01-11 08:23:38 +0100

    Enable gift command again as the bug is fixed

--- src/ROBot.Core/Twitch/Commands/Gift.cs
+++ src/ROBot.Core/Twitch/Commands/Gift.cs
@@ -9,24 +9,24 @@ namespace ROBot.Core.Twitch.Commands
     {
         public override async Task HandleAsync(IBotServer game, ITwitchCommandClient twitch, ICommand cmd)
         {
-            //var channel = cmd.Channel;
-            //var session = game.GetSession(channel);
-            //if (session != null)
-            //{
-            //    var connection = game.GetConnection(session);
-            //    if (connection != null)
-            //    {
-            //        if (string.IsNullOrEmpty(cmd.Arguments) || !cmd.Arguments.Trim().Contains(" "))
-            //        {
-            //            twitch.Broadcast(channel, cmd.Sender.Username, Localization.GIFT_HELP, cmd.Command);
-            //            return;
-            //        }
+            var channel = cmd.Channel;
+            var session = game.GetSession(channel);
+            if (session != null)
+            {
+                var connection = game.GetConnection(session);
+                if (connection != null)
+                {
+                    if (string.IsNullOrEmpty(cmd.Arguments) || !cmd.Arguments.Trim().Contains(" "))
+                    {
+                        twitch.Broadcast(channel, cmd.Sender.Username, Localization.GIFT_HELP, cmd.Command);
+                        return;
+                    }
 
-            //        var player = session.Get(cmd.Sender);
-            //        if (player != null)
-            //            await connection.GiftItemAsync(player, cmd.Arguments);
-            //    }
-            //}
+                    var player = session.Get(cmd.Sender);
+                    if (player != null)
+                        await connection.GiftItemAsync(player, cmd.Arguments);
+                }
+            }
         }
     }
 }
