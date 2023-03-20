2f5699a1734f5ad61747aeccad41aeb63b664095
zerratar <zerratar@gmail.com>
2023-03-20 20:40:23 +0100

    stop chatgpt transformations for a minute if they fail.

--- src/ROBot.Core/OpenAI/ChatGPT35MessageTransformer.cs
+++ src/ROBot.Core/OpenAI/ChatGPT35MessageTransformer.cs
@@ -1,4 +1,6 @@
-﻿using RavenBot.Core;
+﻿using Microsoft.Extensions.Logging;
+using RavenBot.Core;
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -6,44 +8,91 @@ namespace ROBot.Core.OpenAI
 {
     public class ChatGPT35MessageTransformer : IChatMessageTransformer
     {
-        private readonly ROBot.Core.IAppSettings settings;
+        private readonly TimeSpan TransformationTimeout = TimeSpan.FromMinutes(1);
+        private readonly ILogger<ChatGPT35MessageTransformer> logger;
+        private readonly IAppSettings settings;
         private readonly OpenAI openAI;
-
-        public ChatGPT35MessageTransformer(ROBot.Core.IAppSettings settings)
+        private DateTime noTransformationBefore;
+        public ChatGPT35MessageTransformer(
+            ILogger<ChatGPT35MessageTransformer> logger,
+            IAppSettings settings)
         {
+            this.logger = logger;
             this.settings = settings;
             this.openAI = new OpenAI(new OpenAITokenString(settings.OpenAIAuthToken));
         }
 
         public async Task<string> PersonalizeAsync(string message)
         {
-            var completion = await openAI.GetCompletionAsync(message, ChatMessage.Create("system", "You have to make sure that the given sentences are gramatically correct and personal, also take into consideration that any words wrapped with { } are variables and will be changed afterwards so do not remove those. Reply with only the improved result and nothing else. Do not wrap the result in \" even if the input has it."));
-            if (completion == null) return message;
-            return completion.Choices.FirstOrDefault()?.Message.Content ?? message;
+            try
+            {
+                if (DateTime.UtcNow < noTransformationBefore)
+                {
+                    return message;
+                }
+
+                var completion = await openAI.GetCompletionAsync(message, ChatMessage.Create("system", "You have to make sure that the given sentences are gramatically correct and personal, also take into consideration that any words wrapped with { } are variables and will be changed afterwards so do not remove those. Reply with only the improved result and nothing else. Do not wrap the result in \" even if the input has it."));
+                if (completion == null) return message;
+                return completion.Choices.FirstOrDefault()?.Message.Content ?? message;
+            }
+            catch (System.Exception exc)
+            {
+                this.noTransformationBefore = DateTime.UtcNow.Add(TransformationTimeout);
+                logger.LogError("PersonalizeAsync failed for message '" + message + "' Error: " + exc.Message + ", no new messages will be transformed for " + TransformationTimeout.TotalSeconds + " seconds.");
+                return message;
+            }
         }
 
         public async Task<string> TranslateAndPersonalizeAsync(string message, string language)
         {
-            if (string.IsNullOrEmpty(language) || language.StartsWith("none", System.StringComparison.OrdinalIgnoreCase))
+            try
             {
-                return await PersonalizeAsync(message);
-            }
+                if (DateTime.UtcNow < noTransformationBefore)
+                {
+                    return message;
+                }
 
-            var completion = await openAI.GetCompletionAsync(message, ChatMessage.Create("system", "You have to make sure that the given sentences are gramatically correct and personal, also take into consideration that any words wrapped with { } are variables and will be changed afterwards so do not remove those. Reply with only the improved result and nothing else. Do not wrap the result in \" even if the input has it.And make sure its translated into " + language + "."));
-            if (completion == null) return message;
-            return completion.Choices.FirstOrDefault()?.Message.Content ?? message;
+                if (string.IsNullOrEmpty(language) || language.StartsWith("none", System.StringComparison.OrdinalIgnoreCase))
+                {
+                    return await PersonalizeAsync(message);
+                }
+
+                var completion = await openAI.GetCompletionAsync(message, ChatMessage.Create("system", "You have to make sure that the given sentences are gramatically correct and personal, also take into consideration that any words wrapped with { } are variables and will be changed afterwards so do not remove those. Reply with only the improved result and nothing else. Do not wrap the result in \" even if the input has it.And make sure its translated into " + language + "."));
+                if (completion == null) return message;
+                return completion.Choices.FirstOrDefault()?.Message.Content ?? message;
+            }
+            catch (System.Exception exc)
+            {
+                this.noTransformationBefore = DateTime.UtcNow.Add(TransformationTimeout);
+                logger.LogError("TranslateAndPersonalizeAsync failed for message '" + message + "' into langauge '" + language + "'. Error: " + exc.Message + ", no new messages will be transformed for " + TransformationTimeout.TotalSeconds + " seconds.");
+                return message;
+            }
         }
 
         public async Task<string> TranslateAsync(string message, string language)
         {
-            if (string.IsNullOrEmpty(language) || language.StartsWith("none", System.StringComparison.OrdinalIgnoreCase))
+            try
             {
+                if (DateTime.UtcNow < noTransformationBefore)
+                {
+                    return message;
+                }
+
+                if (string.IsNullOrEmpty(language) || language.StartsWith("none", System.StringComparison.OrdinalIgnoreCase))
+                {
+                    return message;
+                }
+
+                var completion = await openAI.GetCompletionAsync(message, ChatMessage.Create("system", "Translate the given sentences into " + language + ", take into consideration that any words wrapped with { } are variables and will be changed afterwards so do not remove those. Reply with only the improved result and nothing else. Do not wrap the result in \" even if the input has it."));
+                if (completion == null) return message;
+                return completion.Choices.FirstOrDefault()?.Message.Content ?? message;
+            }
+            catch (System.Exception exc)
+            {
+                this.noTransformationBefore = DateTime.UtcNow.Add(TransformationTimeout);
+                logger.LogError("TranslateAsync failed for message '" + message + "' into langauge '" + language + "'. Error: " + exc.Message + ", no new messages will be transformed for " + TransformationTimeout.TotalSeconds + " seconds.");
                 return message;
             }
-
-            var completion = await openAI.GetCompletionAsync(message, ChatMessage.Create("system", "Translate the given sentences into " + language + ", take into consideration that any words wrapped with { } are variables and will be changed afterwards so do not remove those. Reply with only the improved result and nothing else. Do not wrap the result in \" even if the input has it."));
-            if (completion == null) return message;
-            return completion.Choices.FirstOrDefault()?.Message.Content ?? message;
         }
     }
 }
